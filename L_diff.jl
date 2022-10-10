cdcf6bdab1998e299dec9ffc1354e507a884249a
Cristiano Ricci <cristiano.ricci6@gmail.com>
2022-10-10 15:30:46 +0200

    changed turbo to tturbo

--- L_diff.jl
+++ L_diff.jl
@@ -24,16 +24,16 @@ function Δ(u,p) return Δ(u,p.Nx,p.Ny,p.Δx) end
 function Δ(u,Nx,Ny,Δx)
 
     Δu = similar(u)
-    @turbo for i in 2:Nx-1, j in 2:Ny-1
+    @tturbo for i in 2:Nx-1, j in 2:Ny-1
         Δu[i,j] = 1/(Δx)^2 * (u[i-1,j] + u[i+1,j] + u[i,j-1] + u[i,j+1] - 4*u[i,j])
     end
 
     # dirichlet 0
-    @turbo for i in 2:Nx-1   
+    @tturbo for i in 2:Nx-1   
         Δu[i,1] = 1/(Δx)^2 * (u[i-1,1] + u[i+1,1] + u[i,2] - 4*u[i,1])
         Δu[i,Ny] = 1/(Δx)^2 * (u[i-1,Ny] + u[i+1,Ny] + u[i,Ny-1] - 4*u[i,Ny])
     end
-    @turbo for j = 2:Ny-1
+    @tturbo for j = 2:Ny-1
         Δu[1,j] = 1/(Δx)^2 * (u[2,j] + u[1,j-1] + u[1,j+1] - 4*u[1,j])
         Δu[Nx,j] = 1/(Δx)^2 * (u[Nx-1,j] + u[Nx,j-1] + u[Nx,j+1] - 4*u[Nx,j])
     end
@@ -50,12 +50,12 @@ function ∂x(u,p) return ∂x(u,p.Nx,p.Ny,p.Δx) end
 function ∂x(u,Nx,Ny,Δx)
 
     ∂x = similar(u)
-    @turbo for i in 1:Nx, j in 2:Ny-1
+    @tturbo for i in 1:Nx, j in 2:Ny-1
         ∂x[i,j] = 1/(2*Δx) * (u[i,j+1] - u[i,j-1])
     end
 
     # dirichlet 0
-    @turbo for i in 1:Nx
+    @tturbo for i in 1:Nx
         ∂x[i,1] =  1/(2*Δx) * u[i,2] 
         ∂x[i,Ny] = - 1/(2*Δx) * u[i,Ny-1]
     end
@@ -67,12 +67,12 @@ function ∂y(u,p) return ∂y(u,p.Nx,p.Ny,p.Δx) end
 function ∂y(u,Nx,Ny,Δx)
 
     ∂y = similar(u)
-    @turbo for i in 2:Nx-1, j in 1:Ny
+    @tturbo for i in 2:Nx-1, j in 1:Ny
         ∂y[i,j] = 1/(2*Δx) * (u[i-1,j] - u[i+1,j])
     end
 
     # dirichlet 0
-    @turbo for j in 1:Ny
+    @tturbo for j in 1:Ny
         ∂y[1,j] =  - 1/(2*Δx) * u[2,j] 
         ∂y[Nx,j] = 1/(2*Δx) * u[Nx-1,j]
     end
